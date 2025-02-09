6b04dc1bfeb3796494ed4de487957c15535851e0
JISUNG PARK <meowbutlerdev@gmail.com>
2025-02-09 16:18:48 +0900

    Initial Commit

---
+++ ToDoReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoReminder
+//
+//  Created by 박지성 on 2/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDoReminderTests/ToDoReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoReminderTests.swift
+//  ToDoReminderTests
+//
+//  Created by 박지성 on 2/9/25.
+//
+
+import Testing
+@testable import ToDoReminder
+
+struct ToDoReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
