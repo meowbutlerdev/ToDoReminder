19389951071ea995c2f81b10b9abd6be32be6b02
JISUNG PARK <meowbutlerdev@gmail.com>
2025-02-11 16:05:59 +0900

    feat: local notification을 위한 권한 요청

--- ToDoReminder/TableViewController.swift
+++ ToDoReminder/TableViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Foundation
+import UserNotifications
 
 class TableViewController: UITableViewController, AddToDoDelegate {
     var toDos: [ToDo] = []
@@ -51,8 +52,42 @@ class TableViewController: UITableViewController, AddToDoDelegate {
         }
     }
 
+    func requestNotificationPermission() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, _ in
+            if !granted {
+                DispatchQueue.main.async {
+                    self.showSettingsAlert()
+                }
+            }
+        }
+    }
+
+    func showSettingsAlert() {
+        let alert = UIAlertController(
+            title: "알림이 거부되었습니다.",
+            message: "알림을 받으려면 설정에서 권한을 허용해주세요.",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(
+            title: "설정으로 이동",
+            style: .default
+        ) { _ in
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(url)
+            }
+        })
+        alert.addAction(UIAlertAction(
+            title: "취소",
+            style: .cancel
+        ))
+        present(alert, animated: true)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        requestNotificationPermission()
 
         setNoDataLabel()
     }
